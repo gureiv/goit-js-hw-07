f6ddc5b992fe920301f1f2e4f8a9448f812c021f
Gurei Vitaliy <gureivitalii@gmail.com>
2020-10-15 21:34:36 +0300

    finish

--- task6.js
+++ task6.js
@@ -1,7 +1,7 @@
 const inputEl = document.querySelector("#validation-input");
 inputEl.addEventListener("blur", inputFocus);
 function inputFocus() {
-  if (inputEl.value.length < inputEl.dataset.length) {
+  if (inputEl.value.length <= inputEl.dataset.length) {
     inputEl.classList.add("valid");
     inputEl.classList.remove("invalid");
   } else {
