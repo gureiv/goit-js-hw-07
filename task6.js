496b91192c8bfbdb7654572fbf72c68411eded0d
Gurei Vitaliy <gureivitalii@gmail.com>
2020-10-26 23:08:57 +0200

    Update task6.js

--- task6.js
+++ task6.js
@@ -1,13 +1,11 @@
-const inputAs = document.querySelector("#validation-input");
-function inputFocus() {
-  if (inputAs.value.length === inputAs.dataset.length) {
-    inputAs.classList.add("valid");
-    inputAs.classList.remove("invalid");
-  } if(inputAs.value.length != inputAs.dataset.length) {
-    inputAs.classList.remove("valid");
-    inputAs.classList.add("invalid");
-  } if (inputAs.value.length === 0) {
-    inputAs.classList.remove("invalid");
+const inputValueEl = document.querySelector("#validation-input");
+inputValueEl.addEventListener("blur", validation);
+function validation() {
+  if (inputValueEl.value.length === inputValueEl.dataset.length) {
+    inputValueEl.classList.remove("invalid");
+    inputValueEl.classList.add("valid");
+  }else {
+    inputValueEl.classList.add("invalid");
+    inputValueEl.classList.remove("valid");
   }
 }
-inputAs.addEventListener("blur", inputFocus);
